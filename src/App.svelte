6d13f1326949994407625dbfefa87c0a86293bb6
Isaac Tait <isaac@mountaintopcoding.com>
2021-03-12 20:44:07 -0800

    Lots of photos and the spreadsheet too

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,7 @@
 <script>
 	export let bus;
 	export let name;
-	let sunsetPhoto = '/familyPhotoSunset.jpg'
+	let sunsetPhoto = '/familyPhotoSunset.jpeg'
 </script>
 
 <main>
@@ -15,13 +15,20 @@
 		<li>New battery (March 2021).</li>
 		<li>Rebuilt brakes (June 2020).</li>
 		<li>New rear drum brakes shoes.</li>
-		<li>All new master cylinder.</li>
+		<li>All new master cylinder..</li>
 		<li>Two new brake fluid reservoirs (one in the cab, the other attached to the master cylinder).</li>
-		<li>New spring kit installed rear brakes.</li>
+		<li>New spring kit installed on the rear brakes.</li>
 		<li>New rear brake calipers</li>
+		<li>Window rubber replaced in 2013 (still looking good as she was in storage with a car cover), metal work also completed to repair rust in the window well areas.</li>
+		<li>Runs strong.</li>
+		<li>Lights work but it is time to upgrade the sound system.</li>
+		<li>Engine compartment hatch has a lock with VW logo key.</li>
+		<li>Comes with a really cool hammock system that installs above the driver and passenger seats. Perfect for storing luggage out of the way at night or even a bed for a small child or two.</li>
 		<li>1600cc Dual-port Dual Carb (Baby Webbers).</li>
 		<li>Curtains are origally from an old college buddies 1972 VW Bus.</li>
 		<li>Upholstery is in great condition.</li>
+		<li>Comes with a loaded toolbox, timing light, and a slew of miscelaneous parts and pieces...</li>
+		<li>I kept very detailed logs of repairs and driving mileage up until Donna went into storage. You can download the spreadsheat <a href="https://volkswagen-forsale-1969.s3-us-west-1.amazonaws.com/prof/Donna+Log.ods target="_blank" rel="noopener noreferrer"">here</a></li>
 		<li>And lots lots more!!!</li>
 	</ul>
 
@@ -32,15 +39,40 @@
 
 	<hr />
 
-	<p>Below are a collection of photos of {name} </p>
+	<p>Below is a collection of photos of {name} spanning the years that she was a part of our family. From the deserts of western Utah to the backroads of California and beyond...</p>
 	<div class="row">
-		<img class="column" src="https://volkswagen-forsale-1969.s3-us-west-1.amazonaws.com/torque+wrench.jpg" alt="repairing brakes" />
-		<img class="column" src="https://volkswagen-forsale-1969.s3-us-west-1.amazonaws.com/Master+Cylinder.jpg" alt="New master cylinder" />
-		<img class="column" src="https://volkswagen-forsale-1969.s3-us-west-1.amazonaws.com/wheel+removed.jpg" alt="" />
-		<img class="column" src="https://volkswagen-forsale-1969.s3-us-west-1.amazonaws.com/Family+Photos+3.jpg" alt="" />
+		<img class="column" src="https://volkswagen-forsale-1969.s3-us-west-1.amazonaws.com/Donna+in+the+Mountains.jpg" alt="" />
+		<img class="column" src="https://volkswagen-forsale-1969.s3-us-west-1.amazonaws.com/Artwork+Highway.jpg" alt="" />
+		<img class="column" src="https://volkswagen-forsale-1969.s3-us-west-1.amazonaws.com/Dashboard.jpg" alt="" />
+		<img class="column" src="https://volkswagen-forsale-1969.s3-us-west-1.amazonaws.com/From+the+crag.jpg" alt="" />
+		<img class="column" src="https://volkswagen-forsale-1969.s3-us-west-1.amazonaws.com/High+Sierra+Campsite.jpg" alt="" />
+		<img class="column" src="https://volkswagen-forsale-1969.s3-us-west-1.amazonaws.com/I80+in+Utah.jpg" alt="" />
+		<img class="column" src="https://volkswagen-forsale-1969.s3-us-west-1.amazonaws.com/Little+Cottonwood+Canyon.jpg" alt="" />
+		<img class="column" src="https://volkswagen-forsale-1969.s3-us-west-1.amazonaws.com/Say+cheese.jpg" alt="" />
+		<img class="column" src="https://volkswagen-forsale-1969.s3-us-west-1.amazonaws.com/Salt+Flats.jpg" alt="" />
+		<img class="column" src="https://volkswagen-forsale-1969.s3-us-west-1.amazonaws.com/Gear.jpg" alt="" />
+		<img class="column" src="https://volkswagen-forsale-1969.s3-us-west-1.amazonaws.com/High+Sierra.jpg" alt="" />
+		<img class="column" src="https://volkswagen-forsale-1969.s3-us-west-1.amazonaws.com/1)+My+Bus+Pic+%232.jpg" alt="" />
 	</div>
-
-
+	<div class="row">
+		<p>Photos taken during the recent brake job. You can read all about it on The Samba <a href="https://www.thesamba.com/vw/forum/viewtopic.php?p=9445749&highlight=#9445749" target="_blank" rel="noopener noreferrer">here</a></p>
+		<img class="column" src="https://volkswagen-forsale-1969.s3-us-west-1.amazonaws.com/Brake+exposed.jpg" alt="" />
+		<img class="column" src="https://volkswagen-forsale-1969.s3-us-west-1.amazonaws.com/finally+solved.jpg" alt="" />
+		<img class="column" src="https://volkswagen-forsale-1969.s3-us-west-1.amazonaws.com/Master+Cylinder.jpg" alt="" />
+		<img class="column" src="https://volkswagen-forsale-1969.s3-us-west-1.amazonaws.com/torque+wrench.jpg" alt="" />
+		<img class="column" src="https://volkswagen-forsale-1969.s3-us-west-1.amazonaws.com/wheel+removed.jpg" alt="" />
+		<img class="column" src="" alt="" />
+	</div>
+	<div class="row">
+		<p>Last summer we hired a professional photographer to do family portraits with Donna as the main prop.</p>
+		<img class="column" src="https://volkswagen-forsale-1969.s3-us-west-1.amazonaws.com/prof/Backside.jpeg" alt="" />
+		<img class="column" src="https://volkswagen-forsale-1969.s3-us-west-1.amazonaws.com/prof/front.jpeg" alt="" />
+		<img class="column" src="https://volkswagen-forsale-1969.s3-us-west-1.amazonaws.com/prof/Hammock.jpeg" alt="" />
+		<img class="column" src="https://volkswagen-forsale-1969.s3-us-west-1.amazonaws.com/prof/Table.jpeg" alt="" />
+		<img class="column" src="" alt="" />
+		<img class="column" src="" alt="" />
+	</div>
+		
 	<div id='location'>
 		<p>Donna currently resides in the warm and sunny beach city of Oceanside, California</p>
 	</div>
@@ -135,9 +167,10 @@
 
 	 .column {
 		max-width: 250px;
+		max-height: 200px;
 		border-radius: 20px;
-		float: left;
-		width: 33.33%;
+		float: center;
+		width:fit-content;
 		padding: 5px;
 		transition: transform 0.25s ease;
 	}
