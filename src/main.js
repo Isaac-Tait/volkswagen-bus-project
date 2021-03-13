148790325ecf97c40cb6ac8c1938816ca1a260bc
Isaac Tait <isaac@mountaintopcoding.com>
2021-03-13 09:16:12 -0800

    bitcoin converter is way to complicated right now

--- src/main.js
+++ src/main.js
@@ -1,14 +1,5 @@
 import App from './App.svelte';
 
-// const api = 'https://apiv2.bitcoinaverage.com/indices/local/ticker/short?crypto=BTC&fiat=USD'
-// $.get(api, p => {
-//   document.querySelector('pre').textContent = JSON.stringify(p, null, 2)
-// }); 
-
-// bitcoin: conversion = () => {
-// 	return (p.BTCUSD.ask * 15000
-// 	)}
-
 const app = new App({
 	target: document.body,
 	props: {
